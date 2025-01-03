f425aa1ce865d4d775df1cf37d0d668c03d512b3
DevCompleteStudios <jorge.yael333@gmail.com>
2025-01-03 12:11:06 -0600

    Agregando entidad de alumnos

---
+++ src/main/java/com/microservicio/commons/alumnos/CommonsAlumnosApplication.java
@@ -0,0 +1,8 @@
+package com.microservicio.commons.alumnos;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class CommonsAlumnosApplication {}
